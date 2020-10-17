b61bf86db92924cce0eccf54d5e6485e3ca40edd
cmatsuta <cmatsuta@jbs.com>
2020-10-17 12:33:01 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// Require the Express
+var express = require('express');
+
+// Create an express server.
+var app = express();
+
+// Set an initial port.
+var PORT = process.env.PORT || 3000;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// ROUTES
+// Point the server to a series of route files .
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// Listen on port.
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
